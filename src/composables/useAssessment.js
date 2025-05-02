c464f0f0378b4c1c4f2a73a7beba7f9d2f91fa81
Einherjar <rodrigo.silva1186@etec.sp.gov.br>
2025-05-02 16:50:47 -0300

    remove log

--- src/composables/useAssessment.js
+++ src/composables/useAssessment.js
@@ -8,14 +8,9 @@ const { getProfile } = useProfile();
 export const useAssessment = () => {
   async function getAssessment() {
     try {
-      console.log("chegou");
-
       const assessments = await readProfileGovernanceContract(
         "getAllAssessments"
       );
-      console.log("chegou 2");
-      console.log(assessments);
-
       if (assessments.length == 0) return { data: [], success: true };
       const data = [];
       for (const assessment of assessments) {
